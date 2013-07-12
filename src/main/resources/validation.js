926e7c56747d57dd159fd50aeda3fa2a197bad65
takezoe <takezoe@gmail.com>
2013-07-12 15:33:13 +0900

    Fix css classname and support HTML5 formaction attribute.

--- src/main/resources/validation.js
+++ src/main/resources/validation.js
@@ -2,6 +2,12 @@ $(function(){
   $.each($('form[validate=true]'), function(i, form){
     $(form).submit(validate);
   });
+  $.each($('input[formaction]'), function(i, input){
+    $(input).click(function(){
+      var form = $(input).parents('form')
+      $(form).attr('action', $(input).attr('formaction'))
+    });
+  });
 });
 
 function validate(e){
@@ -13,12 +19,13 @@ function validate(e){
 
   $.post(form.attr('action') + '/validate', $(e.target).serialize(), function(data){
     // clear all error messages
-    $('.error-message').text('');
+    $('.error').text('');
     
     if($.isEmptyObject(data)){
       form.data('validated', true);
       form.submit();
     } else {
+      form.data('validated', false);
       displayErrors(data);
     }
   }, 'json');
