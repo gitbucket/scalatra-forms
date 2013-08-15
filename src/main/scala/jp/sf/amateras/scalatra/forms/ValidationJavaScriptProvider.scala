ad13ccb8d339deee6245e91e7b3fa51819962425
takezoe <takezoe@gmail.com>
2013-08-15 17:32:36 +0900

    Add a controller which provides validation.js.

---
+++ src/main/scala/jp/sf/amateras/scalatra/forms/ValidationJavaScriptProvider.scala
@@ -0,0 +1,41 @@
+package jp.sf.amateras.scalatra.forms
+
+import org.scalatra._
+
+/**
+ * Provides JavaScript file which contains some functions for client side validation.
+ * 
+ * You can map this servlet to the path which you want as following:
+ * 
+ * {{{
+ * class ScalatraBootstrap extends LifeCycle {
+ *   override def init(context: ServletContext) {
+ *     context.mount(new JavaScriptProvider, "/commons/js")
+ *     ...
+ *   }
+ * }
+ * }}}
+ * 
+ * In this case, you can refer validation.js in HTML as following:
+ * 
+ * {{{
+ * &lt;link href="/common/js/validation.js" rel="stylesheet"&gt;
+ * }}}
+ */
+class ValidationJavaScriptProvider extends ScalatraServlet {
+  
+  var source: String = null
+  val in = Thread.currentThread.getContextClassLoader.getResourceAsStream("validation.js")
+  try {
+    val bytes = new Array[Byte](in.available)
+    in.read(bytes)
+    source = new String(bytes, "UTF-8")
+  } finally {
+    in.close
+  }
+
+  get("/validation.js"){
+    source
+  }
+
+}
